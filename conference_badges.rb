6114316edd9c235bb84e835311dcd047c47fd73d
elisa <elisaopalka@gmail.com>
2020-11-14 22:50:53 -0500

    commit tree d4d17b5ec012341d0f999da421173c1abaa5d457

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map do |name|("Hello, my name is #{name}.")
+end
+end
+
+def assign_rooms(attendees)
+    rooms = []
+ attendees.each.with_index(1) do |speaker, index|
+    rooms.push("Hello, #{speaker}! You'll be assigned to room #{index}!")
+ end
+ rooms
+end
+
+def printer(attendees)
+    badges = batch_badge_creator(attendees)
+    rooms = assign_rooms(attendees)
+    badges.each do |hello|
+        puts hello
+    end
+    rooms.each do |helloz|
+        puts helloz
+    end
+end
